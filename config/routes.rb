ba3035fed6914acc97a3823a576fcf72f5654f0e
danieldenton <danielmdenton@gmail.com>
2022-09-12 22:55:43 -0700

    ids route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
 root "articles#index"
 
   get "/articles", to: "articles#index"
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/articles/:id", to: "articles#show"
 end
